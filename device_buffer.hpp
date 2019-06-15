cd92bc10a8e6970a964c91bd736edeb7e7b5a466
Jake Hemstad <jhemstad@nvidia.com>
2019-06-14 19:30:05 -0700

    Add accessors for device_buffer.

--- device_buffer.hpp
+++ device_buffer.hpp
@@ -8,13 +8,19 @@
 
 namespace rmm {
 
+/**---------------------------------------------------------------------------*
+ * @file device_buffer.hpp
+ * @brief RAII construct for device memory allocation
+ *---------------------------------------------------------------------------**/
+
 class device_buffer {
  public:
   /**---------------------------------------------------------------------------*
    * @brief Constructs a new device buffer of `size` bytes
    *
-   * @param _size Size in bytes to allocate in device memory
-   * @param stream Optional stream to use for allocation
+   * @param size Size in bytes to allocate in device memory
+   * @param stream CUDA stream on which memory may be allocated if the memory
+   * resource supports streams, else the null stream is used.
    * @param mr Memory resource to use for the device memory allocation
    *---------------------------------------------------------------------------**/
   device_buffer(std::size_t size, cudaStream_t stream = 0,
@@ -41,8 +47,10 @@ class device_buffer {
    * @param other
    *---------------------------------------------------------------------------**/
   device_buffer(device_buffer&& other)
-      : _size{other._size}, _stream{other._stream}, _mr{other._mr} {
-    _data = other._data;
+      : _data{other._data},
+        _size{other._size},
+        _stream{other._stream},
+        _mr{other._mr} {
     other._data = nullptr;
     other._size = 0;
     other._stream = 0;
@@ -79,8 +87,8 @@ class device_buffer {
       _mr->deallocate(_data, _size, _stream);
       _data = other._data;
       _size = other._size;
-      _mr = other._mr;
       _stream = other._stream;
+      _mr = other._mr;
 
       other._data = nullptr;
       other._size = 0;
@@ -90,12 +98,47 @@ class device_buffer {
     return *this;
   }
 
-  ~device_buffer() { _mr->deallocate(_data, _size, _stream); }
+  ~device_buffer() {
+    _mr->deallocate(_data, _size, _stream);
+    _data = nullptr;
+    _size = 0;
+    _stream = 0;
+    _mr = nullptr;
+  }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Returns raw pointer to underlying device memory allocation
+   *---------------------------------------------------------------------------**/
+  void const* data() const { return _data; }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Returns raw pointer to underlying device memory allocation
+   *---------------------------------------------------------------------------**/
+  void* data() { return _data; }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Returns size in bytes of device memory allocation
+   *---------------------------------------------------------------------------**/
+  std::size_t size() const { return _size; }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Returns stream used for allocation/deallocation
+   *---------------------------------------------------------------------------**/
+  cudaStream_t stream() const { return _stream; }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Returns pointer to the memory resource used to allocate and
+   * deallocate the device memory
+   *---------------------------------------------------------------------------**/
+  mr::device_memory_resource const * memory_resource() const { return _mr; }
 
  private:
-  void* _data{nullptr};
-  std::size_t _size{0};
-  cudaStream_t _stream{0};
-  mr::device_memory_resource* _mr{mr::get_default_resource()};
+  void* _data{nullptr};     ///< Pointer to device memory allocation
+  std::size_t _size{0};     ///< Size in bytes of device memory allocation
+  cudaStream_t _stream{0};  ///< Stream which may be used for
+                            ///< allocation/deallocation of device memory
+  mr::device_memory_resource* _mr{
+      mr::get_default_resource()};  ///< The memory resource used to
+                                    ///< allocate/deallocate device memory
 };
 }  // namespace rmm
--- example.cu
+++ example.cu
@@ -1,10 +1,9 @@
 // compile with `nvcc --std=c++14 example.cu`
 
+#include <cassert>
+#include <cstdio>
 #include "default_memory_resource.hpp"
 #include "device_buffer.hpp"
-
-#include <cstdio>
-#include <cassert>
 
 constexpr int size{10};
 
@@ -21,15 +20,11 @@ __global__ void print(int* data) {
 }
 
 int main(void) {
-  rmm::mr::device_memory_resource* resource = rmm::mr::get_default_resource();
   std::size_t size_in_bytes = size * sizeof(int);
+  rmm::device_buffer buff(size_in_bytes);
 
-  void* data = resource->allocate(size_in_bytes);
-
-  init<<<1, 256>>>(static_cast<int*>(data));
-  print<<<1, 256>>>(static_cast<int*>(data));
+  init<<<1, 256>>>(static_cast<int*>(buff.data()));
+  print<<<1, 256>>>(static_cast<int*>(buff.data()));
 
   assert(cudaSuccess == cudaDeviceSynchronize());
-
-  resource->deallocate(data, size_in_bytes);
 }
